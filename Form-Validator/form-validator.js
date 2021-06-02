46e11e4af1a2dde64da32a2edf71be3ac468b375
Alex Santamaria <santamariaalex146@gmail.com>
2021-06-02 08:30:29 -0500

    blurred loading page

--- Form-Validator/form-validator.js
+++ Form-Validator/form-validator.js
@@ -20,6 +20,7 @@ function showSuccess (input){
 }
 
 //check email is valid
+//https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 function checkEmail(input){
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(input.value.trim())){
