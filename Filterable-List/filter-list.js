02fb4098246b9ae7460da8867e009973498527fb
Alex Santamaria <santamariaalex146@gmail.com>
2021-06-07 00:49:15 -0500

    finished filtered list

--- Filterable-List/filter-list.js
+++ Filterable-List/filter-list.js
@@ -1,6 +1,23 @@
 let filterInput = document.getElementById('filterInput');
 
 filterInput.addEventListener('keyup', function filterNames(){
-    console.log(1);
+    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+
+    //get Ul
+    let ul = document.getElementById('names');
+    // get li's from ul, puts li in an array
+    let li = ul.querySelectorAll('li.collection-item');
+
+    //loop through colledction item li's
+    for (let i = 0; i< li.length; i++){
+        let a = li[i].getElementsByTagName('a')[0];
+        // if matches
+        // set to > -1 bc in arrays, a -1 means there isn't that value in the array.
+        if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
 });
 
