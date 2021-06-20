7b05d8776b5546d0e69653f68e9fe1b2745bbb60
Alex Santamaria <santamariaalex146@gmail.com>
2021-06-20 00:27:59 -0500

    completed clock project

--- Clock/clock.js
+++ Clock/clock.js
@@ -20,3 +20,35 @@ toggle.addEventListener('click', (e) => {
         e.target.innerHTML = 'Light Mode';
     }
 })
+
+
+function setTime(){
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const date = time.getDate();
+    const hours = time.getHours();
+    const hourForClock = hours % 12;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hourForClock, 0, 11, 0, 360)}deg)`
+
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+    timeEl.innerHTML = `${hourForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime();
+
+//makes seconds hand function
+setInterval(setTime, 1000);
