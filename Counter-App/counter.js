97ad0103530f881919a1a245448e8ce73e47e2c1
Alex Santamaria <santamariaalex146@gmail.com>
2021-06-23 00:31:26 -0500

    small app that counts up or down when clicked

---
+++ Counter-App/counter.js
@@ -0,0 +1,26 @@
+const count = document.querySelector('.count');
+const buttons = document.querySelector('.buttons');
+
+buttons.addEventListener('click', (e) => {
+    if (e.target.classList.contains('add')){
+        count.innerHTML++;
+    }
+    if (e.target.classList.contains('subtract')){
+        count.innerHTML--;
+    }
+    if (e.target.classList.contains('reset')){
+        count.innerHTML = 0;
+    }
+
+    setColor();
+})
+
+function setColor(){
+    if (count.innerHTML > 0){
+        count.style.color = 'limegreen';
+    } else if (count.innerHTML <0){
+        count.style.color = 'orangered';
+    } else {
+        count.style.color = 'white';
+    }
+}
