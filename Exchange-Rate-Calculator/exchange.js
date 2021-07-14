d68272c5e1668be2f0e4c431d5fab35e358a3646
Alex Santamaria <santamariaalex146@gmail.com>
2021-07-13 21:09:22 -0500

    implemented exchange rate API

--- Exchange-Rate-Calculator/exchange.js
+++ Exchange-Rate-Calculator/exchange.js
@@ -13,7 +13,17 @@ function calculate(){
     const currency_one = currencyEl_one.value;
     const currency_two = currencyEl_two.value;
 
-    fetch(``)
+    // create account and use personal API, and update to latest version of API
+    // https://www.exchangerate-api.com/
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+        .then(res => res.json())
+        .then(data => {
+            const rate = data.rates[currency_two];
+
+            rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+            amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+        })
 }
 
 
@@ -22,5 +32,12 @@ currencyEl_one.addEventListener('change', calculate);
 amountEl_one.addEventListener('input', calculate);
 currencyEl_two.addEventListener('change', calculate);
 amountEl_two.addEventListener('input', calculate);
+
+swap.addEventListener('click', () => {
+    const temporary = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temporary;
+    calculate();
+})
 
 calculate();
